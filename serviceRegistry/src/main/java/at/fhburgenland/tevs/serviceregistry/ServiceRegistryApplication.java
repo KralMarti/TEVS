dcb2cecb2628d525595a3048fe0bf94f460efa4f
Martin Kral <2110859031@fh-burgenland.at>
2023-06-08 16:56:56 +0200

    add: Configure ServiceRegistry

--- serviceRegistry/src/main/java/at/fhburgenland/tevs/serviceregistry/ServiceRegistryApplication.java
+++ serviceRegistry/src/main/java/at/fhburgenland/tevs/serviceregistry/ServiceRegistryApplication.java
@@ -1,9 +1,11 @@
-package at.fhburgenland.tevs.serviceRegistry;
+package at.fhburgenland.tevs.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
--- serviceRegistry/src/test/java/at/fhburgenland/tevs/serviceregistry/ServiceRegistryApplicationTests.java
+++ serviceRegistry/src/test/java/at/fhburgenland/tevs/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package at.fhburgenland.tevs.serviceRegistry;
+package at.fhburgenland.tevs.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
